94bd06fe83ec09e5325ab985a3b48770b66ec60e
Hashir Baig <hashirbaig@gmail.com>
2019-10-12 19:59:31 +0500

    Updated some Metadata files and images

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,8 @@ module.exports = {
     siteHeadline: `Hashir Baig - Freelancer | iOS & Android Developer | AWS Cloud Solutions`,
     siteDescription: `Welcome to my Portfolio website. I have expertise in Native Android Development, React Native, Flutter, Desktop Apps and a lot more. I can help you in developing and designing a solution for your project using AWS Cloud solutions.`,
     siteLanguage: `en`,
+    siteUrl: `http://www.meethashir.com`,
+    siteImage: `/banner.png`,
     author: `Hashir Baig`,
   },
   plugins: [
@@ -25,25 +27,25 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        name: `Hashir Baig - Mobile Apps | Web | AWS Cloud`,
+        short_name: `Hashir Baig`,
+        description: `Hashir Baig - Freelancer | iOS & Android Developer | AWS Cloud Solutions`,
         start_url: `/`,
         background_color: `#141821`,
         theme_color: `#f6ad55`,
         display: `standalone`,
-        icons: [
-          {
-            src: `/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`,
-          },
-          {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`,
-          },
-        ],
+        // icons: [
+        //   {
+        //     src: `/android-chrome-192x192.png`,
+        //     sizes: `192x192`,
+        //     type: `image/png`,
+        //   },
+        //   {
+        //     src: `/android-chrome-512x512.png`,
+        //     sizes: `512x512`,
+        //     type: `image/png`,
+        //   },
+        // ],
       },
     },
     {
